0a25bcdc6f60ded3bb8b68e03a12f55ccce7371b
Enzo <enzo202p@gmail.com>
2022-10-19 21:42:40 -0300

    feat: rota de user

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import GlobalStyles from "../styles/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./HomePage";
+import UserPosts from "./UserPosts";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/home' element={<Home />}></Route>
+                    <Route path='/user/:id' element={ <UserPosts />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/components/UserPosts.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+
+export default function UserPosts(){
+    const { id } = useParams();
+    const [userData, setUserData] = useState([]);
+    useEffect(() => {
+        const promisse = axios.get('http://localhost:4489/users/1');
+        promisse.then(p => setUserData([...p.data]));
+    },[]);
+    console.log('Renderizando',userData);
+    return(
+        <>
+            <div>
+                <img src={userData[0]?.picture}/>
+                <h1>{userData[0]?.username}</h1>
+            </div>
+        </>
+    )
+}
