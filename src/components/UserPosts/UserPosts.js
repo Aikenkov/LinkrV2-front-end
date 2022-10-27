b0ae56a9f7a73799d43971572c71355ad34a05bc
Gustavo <gumrqss@gmail.com>
2022-10-27 19:22:17 -0300

    button follow

--- src/components/UserPosts/UserPosts.js
+++ src/components/UserPosts/UserPosts.js
@@ -3,15 +3,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import Posts from "../Posts/Posts";
 import styled from "styled-components";
 import Trending from "../trending/Trending";
-import { getUser, getUserPosts } from "../Service/api";
+import { getUser, getUserPosts, postFollow, deleteFollow, getFollowing } from "../Service/api";
 
 export default function UserPosts() {
     const { id } = useParams();
     const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
     const [user, setUser] = useState({});
+    const [idSeguido, setIdSeguido] = useState();
+    const [followingUser, setFollowingUser] = useState(false)
+    
     useEffect(() => {
-        console.log("Entrei no use effect");
         const userPromisse = getUser(id);
         userPromisse.then((p) => setUser({ ...p.data }));
         userPromisse.catch((p) => {
@@ -20,7 +22,54 @@ export default function UserPosts() {
         });
         const postsPromisse = getUserPosts(id);
         postsPromisse.then((p) => setPosts([...p.data]));
+
+        getFollowing(id)
+        .then((response)=>{
+            if(response.data.length === 0){
+                setFollowingUser(false)
+            }else{
+                setFollowingUser(true)
+            }
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
     }, []);
+
+
+
+
+    function following(res){
+        if(res === 'follow'){
+            const body = {
+                id:parseInt(id)
+            };
+
+            postFollow(body)
+                .then(() => {
+                    setFollowingUser(true)
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+
+        if(res === 'unfollow'){
+            const body = {
+                id:parseInt(id)
+             };
+            deleteFollow(body)
+                .then(()=>{
+                    setFollowingUser(false);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+    }
+
+  
+   
 
     return (
         <Wrapper>
@@ -31,10 +80,71 @@ export default function UserPosts() {
                 </Title>
                 <Posts func={getUserPosts} param={id} />
             </Page>
-            <Trending />
+            <Content>
+                {
+                    followingUser  ?
+                        <Unfollow onClick={()=>{following('unfollow')}}>
+                            Unfollow
+                        </Unfollow>
+                    :
+                        <Follow onClick={()=>{following('follow')}}>
+                            Follow
+                         </Follow>
+                }
+                <Trending />
+            </Content>  
         </Wrapper>
     );
 }
+const Content = styled.div`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+    position:relative ;
+`
+const Follow = styled.div`
+    width: 40%;
+    height: 40px;
+    display:flex;
+    align-items:center ;
+    justify-content: center ;
+    position: absolute ;
+    top:135px;
+    right:25px;
+    background-color:#1877F2;
+    border-radius:5px;
+
+    font-family: Lato;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 17px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #FFFF;
+    cursor:pointer ;
+
+`
+const Unfollow = styled.div`
+        width: 40%;
+    height: 40px;
+    display:flex;
+    align-items:center ;
+    justify-content: center ;
+    position: absolute ;
+    top:135px;
+    right:25px;
+    background-color:#FFFF;
+    border-radius:5px;
+
+    font-family: Lato;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 17px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #1877F2;
+    cursor:pointer ;
+`
 
 const Wrapper = styled.div`
     display: flex;
