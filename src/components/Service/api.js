fc4364bf0ebf23d050f0b1c519ae1c36cde61c58
Brunna Serafina <brunnaserafina@gmail.com>
2022-10-28 13:31:47 -0300

    fix: link api

--- src/components/Service/api.js
+++ src/components/Service/api.js
@@ -10,8 +10,7 @@ function createHeaders() {
   return config;
 }
 
-//const URL = "https://linkr-back-deploy.herokuapp.com";
-const URL = "http://localhost:5000";
+const URL = "https://linkr-back-deploy.herokuapp.com";
 
 export function postSignUp(body) {
   const promise = axios.post(`${URL}/signup`, body);
